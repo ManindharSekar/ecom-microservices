d0a1a678870de238da468cfac5a2fe661ee34b8d
ManindharSekar <manindharsekar@gmail.com>
2026-01-09 10:37:06 +0530

    Add CartItem Entity

---
+++ ecom-application/src/main/java/com/ecom/app/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.ecom.app.repository;
+
+import com.ecom.app.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem,Long> {
+}
